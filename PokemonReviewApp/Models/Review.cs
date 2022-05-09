8652eb0fa7b611298c8c45d58a3e782505fcc4c1
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-09 16:15:39 -0500

    Extablished database contents

--- PokemonReviewApp/Models/Review.cs
+++ PokemonReviewApp/Models/Review.cs
@@ -3,9 +3,10 @@
     public class Review
     {
         public int Id { get; set; }
-        public string title { get; set; }
+        public string Title { get; set; }
         public string Text { get; set; }
-        public Review Reviewer { get; set; }
+        public int Rating { get; set; }
+        public Reviewer Reviewer { get; set; }
         public Pokemon Pokemon { get; set; }
     }
 }
