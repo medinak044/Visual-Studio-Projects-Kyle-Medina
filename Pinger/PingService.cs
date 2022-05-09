2df4790cd1915651d423a67b83c63fc05902ad01
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-09 08:15:34 -0500

    Built network utility

---
+++ Pinger/PingService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pinger
+{
+    public class PingService
+    {
+        public int Data { get; set; }
+        public byte[] Buffer { get; set; }
+        public int Timeout { get; set; }
+        public string Address { get; set; }
+        public Ping pingSender { get; set; }
+        public PingOptions pingOptions { get; set; }
+
+        public PingService()
+        {
+            Timeout = 120;
+            Address = "4.2.2.2";
+            Buffer = Encoding.ASCII.GetBytes("Learn to Code");
+            pingSender = new Ping();
+            pingOptions = new PingOptions();
+            pingOptions.DontFragment = true;
+        }
+
+        public bool SendPing()
+        {
+            PingReply reply = pingSender.Send(Address, Timeout, Buffer, pingOptions);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                Console.WriteLine($"Response: {"{0}"} {reply.Status.ToString()}");
+                Console.WriteLine($"Roundtrip: {"{0}"} {reply.RoundtripTime}");
+                Console.WriteLine($"Time to live: {"{0}"} {reply.Options.Ttl}");
+                Console.WriteLine($"Buffer size: {"{0}"} {reply.Buffer.Length}");
+                return true;
+            }
+            else { return false; }
+        }
+    }
+}
--- Pinger/Program.cs
+++ Pinger/Program.cs
@@ -1,25 +1,33 @@
-﻿using System.Net.NetworkInformation;
+﻿using Pinger;
+using System.Net.NetworkInformation;
 using System.Text;
 
 
 
 // Pinging Google DNS Server 4.2.2.2
 
-Ping pingSender = new Ping();
-PingOptions options = new PingOptions();
 
-options.DontFragment = true; // In case of errors
+#region Refactored in PingService script
+//Ping pingSender = new Ping();
+//PingOptions options = new PingOptions();
 
-string data = "Learn to code";
-byte[] buffer = Encoding.ASCII.GetBytes(data);
-int timeout = 120;
-string address = "4.2.2.2";
-PingReply reply = pingSender.Send(address, timeout, buffer, options);
+//options.DontFragment = true; // In case of errors
 
-if (reply.Status == IPStatus.Success) 
-{ 
-    Console.WriteLine($"Response: {"{0}"} {reply.Status.ToString()}");
-    Console.WriteLine($"Roundtrip: {"{0}"} {reply.RoundtripTime}");
-    Console.WriteLine($"Time to live: {"{0}"} {reply.Options.Ttl}");
-    Console.WriteLine($"Buffer size: {"{0}"} {reply.Buffer.Length}");
-}
+//string data = "Learn to code";
+//byte[] buffer = Encoding.ASCII.GetBytes(data);
+//int timeout = 120;
+//string address = "4.2.2.2";
+//PingReply reply = pingSender.Send(address, timeout, buffer, options);
+
+//if (reply.Status == IPStatus.Success) 
+//{ 
+//    Console.WriteLine($"Response: {"{0}"} {reply.Status.ToString()}");
+//    Console.WriteLine($"Roundtrip: {"{0}"} {reply.RoundtripTime}");
+//    Console.WriteLine($"Time to live: {"{0}"} {reply.Options.Ttl}");
+//    Console.WriteLine($"Buffer size: {"{0}"} {reply.Buffer.Length}");
+//}
+#endregion
+
+PingService pingService = new PingService();
+var response = pingService.SendPing();
+Console.WriteLine(response);
