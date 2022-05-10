d20c84bd0255104c7ef1b46ebe2b862389778bfc
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-09 21:24:57 -0500

    Stopped at Web API Tutorial #5 due to error

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController
+    {
+        readonly IPokemonRepository _pokemonRepository;
+
+        public PokemonController(IPokemonRepository pokemonRepository) // Shortcut: "ctor"
+        { _pokemonRepository = pokemonRepository; }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid) 
+            { return BadRequest(ModelState); }
+
+            return Ok(pokemons);
+            // Stopped at: https://www.youtube.com/watch?v=-LAeEQSfOQk&list=PL82C6-O4XrHdiS10BLh23x71ve9mQCln0&index=6
+        }
+    }
+}
---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,18 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository: IPokemonRepository
+    {
+        readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        { _context = context; }
+
+        // Database call
+        public ICollection<Pokemon> GetPokemons()
+        { return _context.Pokemon.OrderBy(p => p.Id).ToList(); }
+    }
+}
