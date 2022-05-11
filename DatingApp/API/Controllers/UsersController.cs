154323057913de94d3cd0841d081e194f13b0356
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-11 12:43:31 -0500

    Successfully established endpoints

---
+++ DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        { _context = context; }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers() // Gets multiple users
+        { return _context.Users.ToList(); }
+
+        // api/users/:id
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser() // Gets one user
+        { return  _context.Users.Find(); }
+    }
+}
