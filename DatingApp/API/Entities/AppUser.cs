0dabc7beba0fe8bc04a18ebaad5befce2ad4c145
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-14 15:07:57 -0500

    Fixed database

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,21 @@ namespace Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; } // Hashed password
         public byte[] PasswordSalt { get; set; } // Unique value to decrypt the user's password
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
+
     }
 }
