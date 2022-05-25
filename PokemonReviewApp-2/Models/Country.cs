26bb78228f28ffd259048c12b9d2f99899bfb060
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-24 19:52:22 -0500

    Added model files

---
+++ PokemonReviewApp-2/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace PokemonReviewApp_2.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
