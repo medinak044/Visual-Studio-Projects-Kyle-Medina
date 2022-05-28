065379a116be65346cf8c37b26bff5aaf1f87c9d
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-27 21:57:05 -0500

    Set up database

---
+++ PokemonReviewApp-2/Data/DataContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PokemonReviewApp_2.Models;
+
+namespace PokemonReviewApp_2.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pokemon> Pokemon { get; set; }
+        public DbSet<PokemonOwner> PokemonOwners { get; set; }
+        public DbSet<PokemonCategory> PokemonCategories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Explanation: https://youtu.be/EmV_IBYIlyo?list=PL82C6-O4XrHdiS10BLh23x71ve9mQCln0&t=706
+            // Customize tables without going to the database
+            modelBuilder.Entity<PokemonCategory>()
+                .HasKey(pc => new { pc.PokemonId, pc.CategoryId });
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonCategory>()
+                .HasOne(p => p.Category)
+                .WithMany(pc => pc.PokemonCategories)
+                .HasForeignKey(c => c.CategoryId);
+            
+            modelBuilder.Entity<PokemonOwner>()
+                .HasKey(po => new { po.PokemonId, po.OwnerId });
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Pokemon)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(p => p.PokemonId);
+            modelBuilder.Entity<PokemonOwner>()
+                .HasOne(p => p.Owner)
+                .WithMany(pc => pc.PokemonOwners)
+                .HasForeignKey(c => c.OwnerId);
+        }
+
+    }
+}
---
+++ PokemonReviewApp-2/project-notes.md
@@ -0,0 +1,18 @@
+﻿Packages:
+
+
+----------
+Package Manager Console commands:
+
+PM> Add-Migration InitialCreate 
+- Adds a named migration file
+
+PM> Update-Database
+- Configures database based on migration file(s)
+
+
+Steps:
+- Get packages
+- Set up data structures
+- Link up project to SQL database via connection string (remember to update appsettings.json with the connection string)
+- You can prefill the database with a Seed file
