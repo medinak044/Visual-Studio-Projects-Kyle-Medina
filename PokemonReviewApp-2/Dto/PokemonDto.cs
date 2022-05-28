4725264f104591fb3b1432ee41b63cc780eadd48
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-05-28 15:58:18 -0500

    Created Dto folder

---
+++ PokemonReviewApp-2/Dto/PokemonDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp_2.Dto
+{
+    // Dtos make sure that only the neccessary data from the object model is being sent
+    // back to the user (prevents user sensitive data such as password hashes, payment info)
+    public class PokemonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
--- PokemonReviewApp-2/Program.cs
+++ PokemonReviewApp-2/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using PokemonReviewApp_2;
 using PokemonReviewApp_2.Data;
+using PokemonReviewApp_2.Interfaces;
+using PokemonReviewApp_2.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
+builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
