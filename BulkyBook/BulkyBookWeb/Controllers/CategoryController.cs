3dfc08689ea028bbf303fc941166f01b09bca437
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-06-02 15:06:25 -0500

    Added data context query within razor page

---
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DataContext _context;
+
+        public CategoryController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var objCategoryList = _context.Categories.ToList();
+            return View();
+        }
+    }
+}
