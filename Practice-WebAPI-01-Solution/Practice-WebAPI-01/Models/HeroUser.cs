e34c403baf3014deffb058ead13dc779b47bc82e
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-06-09 13:36:59 -0500

    Added User model

---
+++ Practice-WebAPI-01-Solution/Practice-WebAPI-01/Models/HeroUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice_WebAPI_01.Models
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
