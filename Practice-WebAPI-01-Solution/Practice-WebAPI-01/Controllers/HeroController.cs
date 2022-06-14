deebcaf4f5b6a034f7a029976e557352d8fb1cae
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-06-14 17:19:39 -0500

    Added dependency injection in Program script

--- Practice-WebAPI-01-Solution/Practice-WebAPI-01/Controllers/HeroController.cs
+++ Practice-WebAPI-01-Solution/Practice-WebAPI-01/Controllers/HeroController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Practice_WebAPI_01.Data;
+using Practice_WebAPI_01.Models;
+using Practice_WebAPI_01.Interfaces;
+using AutoMapper;
 
-namespace Practice_WebAPI_01.Controllers
+namespace Practice_WebAPI_01.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HeroController : ControllerBase // Inherit from ControllerBase instead of Controller because Controller just adds support for views (MVC); not applicable to webAPI
 {
-    public class HeroController : Controller
+    private readonly DataContext _context;
+    private readonly IMapper _mapper;
+    private readonly IHeroRepository _heroRepository;
+
+    public HeroController(DataContext context, IMapper mapper, IHeroRepository heroRepository)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _context = context;
+        _mapper = mapper;
+        _heroRepository = heroRepository;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<ICollection<Hero>>> GetHeroes()
+    {
+
+    }
+
 }
--- Practice-WebAPI-01-Solution/Practice-WebAPI-01/Program.cs
+++ Practice-WebAPI-01-Solution/Practice-WebAPI-01/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Practice_WebAPI_01;
 using Practice_WebAPI_01.Data;
+using Practice_WebAPI_01.Interfaces;
+using Practice_WebAPI_01.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 //builder.Services.AddTransient<Seed>();
+builder.Services.AddScoped<IHeroRepository, HeroRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
