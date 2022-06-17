da6e9bffc87956080c304a663c6f616ea55cfa8a
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-06-17 16:10:50 -0500

    Completed unit tests

---
+++ Practice-02-UnitTests/Practice-02-UnitTests/DummyFunction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_02_UnitTests;
+
+public class DummyFunction
+{
+    public string ReturnsPikachuIfZero(int num)
+    {
+        if (num == 0)
+        {
+            return "PIKACHU!";
+        }
+        else
+        {
+            return "Squirtle...";
+        }
+    }
+}
---
+++ Practice-02-UnitTests/Practice-02-UnitTests/Tests/DummyTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_02_UnitTests.Tests;
+
+public class DummyTests
+{
+    // Naming Convention: ClassName_MethodName_ExpectedResult
+    public static void DummyFunction_ReturnsPikachuIfZero_ReturnString()
+    {
+        try
+        {
+            // Arrange: Go get your variables, classes, whatever you need
+            int num = 0;
+            DummyFunction dummyFunc = new DummyFunction();
+
+            // Act: Execute this function/method
+            string result = dummyFunc.ReturnsPikachuIfZero(num);
+
+            // Assert: Whatever is returned, is it what you expected?
+            if (result == "PIKACHU!")
+            {
+                Console.WriteLine("PASSED: DummyFunction_ReturnsPikachuIfZero_ReturnsString");
+            }
+            else
+            {
+                Console.WriteLine("FAILED: DummyFunction_ReturnsPikachuIfZero_ReturnsString");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+}
