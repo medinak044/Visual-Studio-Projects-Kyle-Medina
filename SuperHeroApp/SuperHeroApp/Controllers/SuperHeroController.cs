34f265bd0fc73423f7ca0abeef77db6c9712d117
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-06-22 17:30:59 -0500

    Successfully configured CORS

---
+++ SuperHeroApp/SuperHeroApp/Controllers/SuperHeroController.cs
@@ -0,0 +1,25 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SuperHeroController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<SuperHero>>> GetSuperHeroes()
+    {
+        return new List<SuperHero>
+        {
+            new SuperHero
+            {
+                Name = "SpiderMan",
+                FirstName = "Peter",
+                LastName = "Parker",
+                Place = "New York City"
+            }
+        };
+    }
+}
--- SuperHeroApp/SuperHeroApp/Program.cs
+++ SuperHeroApp/SuperHeroApp/Program.cs
@@ -1,11 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(
+    options =>
+    {
+        options.AddPolicy(
+            name: myAllowSpecificOrigins,
+            policy =>
+            {
+                policy.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+    }
+);
 
 var app = builder.Build();
 
@@ -16,7 +31,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+
+app.UseCors(myAllowSpecificOrigins);
 
 app.UseAuthorization();
 
