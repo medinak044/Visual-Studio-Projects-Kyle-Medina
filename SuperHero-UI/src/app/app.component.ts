6d2b810faef9bb18e4f17cb779db8d5ec623d54a
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-06-23 11:44:54 -0500

    Completed CRUD operations

--- SuperHero-UI/src/app/app.component.ts
+++ SuperHero-UI/src/app/app.component.ts
@@ -8,13 +8,27 @@ import { SuperHeroService } from './services/super-hero.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'SuperHero-UI';
+  title = 'SuperHero-UI'
   heroes: SuperHero[] = []
+  heroToEdit?: SuperHero
 
   constructor(private superHeroService: SuperHeroService) { }
 
   ngOnInit(): void {
-    this.heroes = this.superHeroService.getSuperHeroes()
-    console.log(this.heroes)
+    this.superHeroService
+      .getSuperHeroes()
+      .subscribe((result: SuperHero[]) => (this.heroes = result))
+  }
+
+  updateHeroList(heroes: SuperHero[]) {
+    this.heroes = heroes
+  }
+
+  initNewHero() {
+    this.heroToEdit = new SuperHero
+  }
+
+  editHero(hero: SuperHero) {
+    this.heroToEdit = hero
   }
 }
