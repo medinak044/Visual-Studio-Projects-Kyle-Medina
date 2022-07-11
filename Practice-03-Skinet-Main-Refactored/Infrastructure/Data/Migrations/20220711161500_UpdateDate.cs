0777e97bc23abcdeadaaf48be8c1c366b81570f3
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-07-11 11:18:39 -0500

    Refactored projects to .NET 6

---
+++ Practice-03-Skinet-Main-Refactored/Infrastructure/Data/Migrations/20220711161500_UpdateDate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    public partial class UpdateDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "character varying(180)",
+                maxLength: 180,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(180)",
+                oldMaxLength: 180);
+        }
+    }
+}
