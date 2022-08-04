26459223a914f1b76952f953a4a659710c0530de
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-08-03 19:22:16 -0500

    Successfully seeded data

--- Practice-01-WebAPI-Solution/Practice-WebAPI-01/Program.cs
+++ Practice-01-WebAPI-Solution/Practice-WebAPI-01/Program.cs
@@ -11,7 +11,7 @@ var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 builder.Services.AddControllers();
-//builder.Services.AddTransient<Seed>();
+builder.Services.AddTransient<Seed>(); // dotnet run seeddata
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); // ref: https://youtu.be/K4WuxwkXrIY?list=PL82C6-O4XrHdiS10BLh23x71ve9mQCln0&t=1660
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -39,6 +39,22 @@ builder.Services.AddCors(
 );
 
 var app = builder.Build();
+
+#region Seed data
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+    SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<Seed>();
+        service.SeedDataContext();
+    }
+}
+#endregion
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
