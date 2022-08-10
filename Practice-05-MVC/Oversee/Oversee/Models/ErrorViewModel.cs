812b9325cf127b038f4bbd20f5240a18d366c57d
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-08-10 17:13:50 -0500

    Initial commit

---
+++ Practice-05-MVC/Oversee/Oversee/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Oversee.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
